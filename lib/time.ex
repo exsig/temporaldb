1bf96e745ccdcd54ffad8afb901640aef3f06680
Joseph Wecker <joseph.wecker@gmail.com>
2013-08-26 19:21:45 -0700

    Starting to consolidate different types associated with date/time data

--- lib/time.ex
+++ lib/time.ex
@@ -1,7 +1,37 @@
 defmodule Time do
+  # TODO: Find out if the now_dt is an epoch from year 0 or 1970 and adjust logic appropriately
+
+  # See: https://gist.github.com/zaphar/104903
+  # See:
+
+  @type year       :: non_neg_integer
+  @type month      :: 1..12
+  @type day        :: 1..31
+  @type hour       :: 0..23
+  @type minute     :: 0..59
+  @type second     :: 0..59
+  @type daynum     :: 1..7
+  @type weeknum    :: 1..53
+  @type megasecs   :: non_neg_integer
+  @type secs       :: non_neg_integer
+  @type microsecs  :: non_neg_integer
+
+  @type e_date     :: {year, month, day}
+  @type e_time     :: {hour, minute, second} # erlang time
+
+  #--------- DateTime types --------------------------------------------------
+  @type erl_dt         :: {e_date, e_time}            # erlang datetime tuple (NOTE: No fractions of a second)
+  @type now_dt         :: {megasecs, secs, microsecs} # erlang timestamp tuple such as returned by :erlang.now()
+  @type unix_dt        :: non_neg_integer             # unix epoch            (NOTE: Also no fractions of a second)
+  @type micro_dt       :: non_neg_integer             # microtime integer
+  @type unixhighres_dt :: float                       # unix epoch as float with microtime
+  @type bin_dt         :: <<_ :: 8 * 8>>              # 64 bit binary representation of the micro_dt
+
+
   @gregorian_seconds_1970 62_167_219_200 # == :calendar.datetime_to_gregorian_seconds({{1970,1,1},{0,0,0}})
 
   def now(),                     do: :erlang.now()
+
   def timestamp_to_datetime(ts), do: :calendar.now_to_datetime(ts)
   def datetime_to_timestamp(dt)  do
     gseconds = :calendar.datetime_to_gregorian_seconds(dt)
